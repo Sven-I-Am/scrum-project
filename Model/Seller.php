4446a762c0572f23614c452329abb38f7921ddac
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-12-21 11:21:21 +0100

    Created the set and get functions

--- Model/Seller.php
+++ Model/Seller.php
@@ -4,15 +4,66 @@ declare(strict_types=1);
 
 class Seller
 {
+    private $id;
     private $name;
+    private $adress;
+    private $email;
+    private $password;
 
-    public function __construct(string $name)
-    {
+    public function __construct($id, $name, $adress, $email, $password){
+        $this->id = $id;
         $this->name = $name;
+        $this->adress = $adress;
+        $this->email = $email;
+        $this->password = $password;
     }
 
-    public function getName(): string
-    {
+    // get and set seller's id
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function setId(){
+        $this->id;
+    }
+
+    // get and set seller's name
+
+    public function getName(){
         return $this->name;
     }
+    public function setName(){
+        $this->name;
+    }
+
+    // get and set seller's adress
+    public function getAdress(){
+        return $this->adress;
+    }
+
+    public function setAdress(){
+        $this->adress;
+    }
+
+    // get and set seller's email
+
+    public function getEmail(){
+        return $this->email;
+    }
+
+    public function setEmail(){
+        $this->email;
+    }
+
+    // get and set seller's password
+
+    public function getPassword(){
+        return $this->password;
+    }
+
+    public function setPassword(){
+        $this->password;
+    }
+
 }
