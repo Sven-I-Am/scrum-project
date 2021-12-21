440670129367ab09fc1b0f35201689f880aa6118
unknown <nehalonkar27@gmail.com>
2021-12-21 21:18:36 +0100

    working on footer style

--- View/includes/footer.php
+++ View/includes/footer.php
@@ -1,12 +1,14 @@
-<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <div class="col-md-4 d-flex align-items-center">
-      <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-        <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
-      </a>
-      <span class="text-muted">Copyright © 2021-2022 Pokeball Inc. All Rights Reserved.</span>
-    </div>
+ <!-- Local CSS link -->
+ <link rel="stylesheet" href=".\View\style\footer.css">
 
-    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+<footer class=" footer d-flex flex-wrap justify-content-between align-items-center py-3 my-4">
+    <div class="col-md-6 d-flex align-items-center">
+      <!-- <a href="/" class=" text-center mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+        <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
+      </a> -->
+      <div class="text-muted text-center ">Copyright © 2021-2022 Pokeball Inc. All Rights Reserved.</div>
+    </div>
+     <ul class="nav col-md-4 list-unstyled d-flex m-auto">
       <li class="ms-3"><a class="text-muted" href="http://twitter.com"><img width="30" height="30" src=".\assets\images\twitter.png" alt=""></a></li>
       <li class="ms-3"><a class="text-muted" href="http://instagram.com"><img width="30" height="30" src=".\assets\images\instagram.png" alt=""></a></li>
       <li class="ms-3"><a class="text-muted" href="http://facebook.com"><img width="30" height="30" src=".\assets\images\facebook.png" alt=""></a></li>
