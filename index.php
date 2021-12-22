b101341e13bd4e16a24b9fee673b21fdbc516c98
Sven-I-Am <sven.vander.mierde@gmail.com>
2021-12-22 10:25:18 +0100

    merges and updates to index.php

--- index.php
+++ index.php
@@ -3,11 +3,10 @@
 declare(strict_types=1);
 
 //include all your model files here
-require 'Model/Seller.php';
-require 'Model/SellerLoader.php';
+require 'Model/User.php';
+require 'Model/Product.php';
 //include all your controllers here
 require 'Controller/HomepageController.php';
-require 'Controller/LoginController.php';
 
 //you could write a simple IF here based on some $_GET or $_POST vars, to choose your controller
 //this file should never be more than 20 lines of code!
