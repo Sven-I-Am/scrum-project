e3b5265d61ea413a804ffdcbe5a3fdb8beeb3ae2
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-12-23 12:29:57 +0100

    Create TestIndex.php

---
+++ ProductTest/TestIndex.php
@@ -0,0 +1,11 @@
+
+<form method="post">
+
+<input type="text" name="name">
+<input type="text" name="description">
+<input type="text" name="price">
+<input typr="text" name="image">
+<input type="submit" name="submit">
+
+<input type="submit" name="show">
+</form>
