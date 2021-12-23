c99cf9592a0469104bfc1e98b3cf0bbe06627174
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-12-23 13:55:40 +0100

    removed the code that we don't use

--- ProductTest/TestController.php
+++ ProductTest/TestController.php
@@ -18,21 +18,9 @@ class TestController{
     public function render(array $GET, array $POST){
 
         if(isset($_POST['submit'])){
-            echo "ola";
-        
-           var_dump($_POST['name']); 
-           var_dump($_POST['description']); 
-           var_dump(floatval($_POST['price'])); 
-           var_dump($_POST['image']);
-
-        //    $price = floatval($_POST['price']);
-        
-            // $product = new Product("", $_POST['name'], $_POST['description'], floatval($_POST['price']), false, $_POST['image'], 0, "");
-            // $database = new ProductLoader();
-        
-            // $database->createProduct($product);
         
             ProductLoader::createProduct($this->db, new Product(0, $_POST['name'], $_POST['description'], floatval($_POST['price']), false, $_POST['image'], 0, ""));
+            
         }
     }
     
