a9be52d09ce77957f524d1bf631b98be7943884b
unknown <nehalonkar27@gmail.com>
2021-12-24 10:31:32 +0100

    product view

--- View/product.php
+++ View/product.php
@@ -6,14 +6,17 @@ Anything complex should be calculated in the model -->
         <div class="row row-cols-1 row-cols-md-4 g-4">
             <div class="col">
                 <div class="card h-auto border border-2 border-dark">
-                    <img src=".\assets\screenshots\poke.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
+                   <img src=".\assets\screenshots\poke.jpg" class="card-img-top" alt="...">
+                    <div id="content">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt illo aliquid repellat praesentium animi. Labore dignissimos consectetur sint, eligendi asperiores, reprehenderit hic omnis unde consequatur eaque dolore, dolores excepturi odit.</p>
+                    </div>
+                    
+                     <div class="card-body">
                         <h5 class="card-title gap-2 m-auto text-danger">Pokemon toy</h5>
                         <h5 class="card-title gap-2 mx-auto">Price:</h5>
                          <div class="d-grid gap-2 col-6 mx-auto">
                             <button type="button" class="btn btn-outline-primary">Buy</button>
                         </div>
-
                     </div>
                 </div>
             </div>
