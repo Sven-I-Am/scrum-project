16fe52453d6156ea1bf9d3fa8f85e1e0f44dc2c9
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-12-24 10:36:54 +0100

    Now I can read all products from the database

--- ProductTest/TestController.php
+++ ProductTest/TestController.php
@@ -18,19 +18,44 @@ class TestController{
     public function render(array $GET, array $POST){
 
 
+        // create a product in the database
         // I have to create some validation to receive de information?
+        // I have to change userId for a $_GET['userId']
 
         if(isset($_POST['submit'])){
         
-            ProductLoader::createProduct($this->db, new Product(0, $_POST['name'], $_POST['description'], floatval($_POST['price']), false, $_POST['image'], 0, ""));
+            ProductLoader::createProduct($this->db, new Product(0, $_POST['name'], $_POST['description'], floatval($_POST['price']), false, $_POST['image'], 1, "12"));
             
         }
+
+        //  delete a product from the database
 
         if(isset($_POST['delete'])){
 
             ProductLoader::deleteProduct($this->db, $_GET['id']);
 
             echo "delete";
+        }
+
+        // show all products from the database
+
+        if(isset($_POST['read'])){
+
+            $products = ProductLoader::readAllProducts($this->db);
+
+            foreach($products as $key => $product){
+
+                echo $product->getId()."<br>";
+                echo $product->getName()."<br>";
+                echo $product->getDescription()."<br>";
+                echo $product->getPrice()."<br>";
+                echo $product->getSold()."<br>";
+                echo $product->getImage()."<br>";
+                echo $product->getUserId()."<br>";
+                echo $product->getSellDate()."<br>";
+    
+            }
+            
         }
     }
     
