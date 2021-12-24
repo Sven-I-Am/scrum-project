2191328c8eec0b67be053841603635807aec4714
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-12-24 10:08:32 +0100

    I had a bug in the create method, but now this function is working correctly

--- Loader/ProductLoader.php
+++ Loader/ProductLoader.php
@@ -7,7 +7,12 @@ class ProductLoader{
         
         $products = $PDO->query("SELECT * FROM product");
 
-        return ;
+        foreach($products as $key => $product){
+
+            var_dump($product['price']);
+
+            return new Product($product['id'], $product['name'], $product['description'], $product['price'], $product['sold'], $product['image'], $product['userid'], $product['selldate']);
+        }      
 
     }
 
@@ -18,9 +23,12 @@ class ProductLoader{
         $price = $product->getPrice();
         $sold = $product->getSold();
         $image = $product->getImage();
+        $userId = $product->getUserId();
+        $sellDate = $product->getSellDate();
 
-        $PDO->query("INSERT INTO product (name, description, price, sold, image)
-            VALUES ('$name', '$description', '$price', '$sold', '$image')");
+
+        $PDO->query("INSERT INTO product (name, description, price, sold, image, userid, selldate)
+            VALUES ('$name', '$description', '$price', '$sold', '$image', '$userId', '$sellDate')");
     }
 
     public static function deleteProduct(PDO $PDO, int $id){
