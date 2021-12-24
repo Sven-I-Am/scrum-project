19b96fe287a441657c231f81a04bbd72cfa817e5
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-12-24 12:30:54 +0100

    added some validation.

--- ProductTest/TestIndex.php
+++ ProductTest/TestIndex.php
@@ -1,10 +1,11 @@
 
 <form method="post">
 
-<input type="text" name="name">
-<input type="text" name="description">
-<input type="text" name="price">
-<input typr="text" name="image">
+<input type="text" name="name" <?php if(isset($errors['name'])) { echo 'border-red-500 focus:border-red-000'; } else { echo 'border-gray-300 focus:border-blue-400'; } ?> >
+<input type="text" name="description" <?php if(isset($errors['description'])) { echo 'border-red-500 focus:border-red-000'; } else { echo 'border-gray-300 focus:border-blue-400'; } ?> >
+<input type="text" name="price" <?php if(isset($errors['price'])) { echo 'border-red-500 focus:border-red-000'; } else { echo 'border-gray-300 focus:border-blue-400'; } ?> >
+<input typr="text" name="image" <?php if(isset($errors['image'])) { echo 'border-red-500 focus:border-red-000'; } else { echo 'border-gray-300 focus:border-blue-400'; } ?> >
+
 <input type="submit" name="submit">
 
 <input type="submit" name="delete">
