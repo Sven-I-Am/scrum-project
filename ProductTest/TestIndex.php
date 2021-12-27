f85c2cd1be6c11211ad84eaea70ad6d925ea3b18
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-12-27 16:21:22 +0100

    Update TestIndex.php

--- ProductTest/TestIndex.php
+++ ProductTest/TestIndex.php
@@ -1,20 +1,7 @@
 
-<?php
-require ("./Helper/Sanitize.php");
-    
-    if(isset($_POST) && isset($_POST['submit'])){
-        $product = new TestController();
-        $data = $product->createProduct();
-        if(isset($data['status']) && $data['status'] == 'error') {
-            $errors = $data['errors'];
-        }
-    }
-    
-?>
-
 <form method="post">
 
-<input type="text" name="name">  <?php if(isset($errors['name'])) { echo $errors['name']; } ?>
+<input type="text" name="name"> <span> <?php if(isset($errors['name'])) { echo $errors['name']; } ?> </span>
 <input type="text" name="description"> <?php if(isset($errors['description'])) { echo $errors['description']; } ?>
 <input type="text" name="price"> <?php if(isset($errors['price'])) { echo $errors['price']; } ?>
 <input type="text" name="image" > <?php if(isset($errors['image'])) { echo $errors['image']; } ?>
