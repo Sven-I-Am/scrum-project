3d3ae62ef0a35472f0692b5131adaf0f6052efc5
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-12-27 14:41:54 +0100

    Update TestIndex.php

--- ProductTest/TestIndex.php
+++ ProductTest/TestIndex.php
@@ -14,10 +14,10 @@ require ("./Helper/Sanitize.php");
 
 <form method="post">
 
-<input type="text" name="name" style= <?php if(isset($errors['name']))  { echo "'border-red-500 focus:border-red-000'>".$errors['name']; } ?>
-<input type="text" name="description" class="<?php if(isset($errors['description'])) { echo $errors['description']. 'border-red-500 focus:border-red-000'; } else { echo 'border-gray-300 focus:border-blue-400'; } ?>" >
-<input type="text" name="price" class="<?php if(isset($errors['price'])) { echo $errors['price']. 'border-red-500 focus:border-red-000'; } else { echo 'border-gray-300 focus:border-blue-400'; } ?>" >
-<input typr="text" name="image" class="<?php if(isset($errors['image'])) { echo $errors['image']. 'border-red-500 focus:border-red-000'; } else { echo 'border-gray-300 focus:border-blue-400'; } ?>" >
+<input type="text" name="name">  <?php if(isset($errors['name'])) { echo $errors['name']; } ?>" 
+<input type="text" name="description"> <?php if(isset($errors['description'])) { echo $errors['description']; } ?>
+<input type="text" name="price"> <?php if(isset($errors['price'])) { echo $errors['price']; } ?>
+<input type="text" name="image" > <?php if(isset($errors['image'])) { echo $errors['image']; } ?>
 
 <input type="submit" name="submit">
 
