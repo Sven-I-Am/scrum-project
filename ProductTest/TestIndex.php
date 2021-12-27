8ad9b06aa74805f4d638d6064f121a214afab6ce
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-12-27 11:08:02 +0100

    Update TestIndex.php

--- ProductTest/TestIndex.php
+++ ProductTest/TestIndex.php
@@ -1,10 +1,23 @@
 
+<?php
+require ("./Helper/Sanitize.php");
+    
+    if(isset($_POST) && isset($_POST['submit'])){
+        $product = new TestController();
+        $data = $product->createProduct();
+        if(isset($data['status']) && $data['status'] == 'error') {
+            $errors = $data['errors'];
+        }
+    }
+    
+?>
+
 <form method="post">
 
-<input type="text" name="name" <?php if(isset($errors['name'])) { echo 'border-red-500 focus:border-red-000'; } else { echo 'border-gray-300 focus:border-blue-400'; } ?> >
-<input type="text" name="description" <?php if(isset($errors['description'])) { echo 'border-red-500 focus:border-red-000'; } else { echo 'border-gray-300 focus:border-blue-400'; } ?> >
-<input type="text" name="price" <?php if(isset($errors['price'])) { echo 'border-red-500 focus:border-red-000'; } else { echo 'border-gray-300 focus:border-blue-400'; } ?> >
-<input typr="text" name="image" <?php if(isset($errors['image'])) { echo 'border-red-500 focus:border-red-000'; } else { echo 'border-gray-300 focus:border-blue-400'; } ?> >
+<input type="text" name="name" style= <?php if(isset($errors['name']))  { echo "'border-red-500 focus:border-red-000'>".$errors['name']; } ?>
+<input type="text" name="description" class="<?php if(isset($errors['description'])) { echo $errors['description']. 'border-red-500 focus:border-red-000'; } else { echo 'border-gray-300 focus:border-blue-400'; } ?>" >
+<input type="text" name="price" class="<?php if(isset($errors['price'])) { echo $errors['price']. 'border-red-500 focus:border-red-000'; } else { echo 'border-gray-300 focus:border-blue-400'; } ?>" >
+<input typr="text" name="image" class="<?php if(isset($errors['image'])) { echo $errors['image']. 'border-red-500 focus:border-red-000'; } else { echo 'border-gray-300 focus:border-blue-400'; } ?>" >
 
 <input type="submit" name="submit">
 
