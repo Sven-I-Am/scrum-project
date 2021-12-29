163b09e6ceda6ee6d7be2583a89fa95cc26586e8
Sven <sven.vander.mierde@gmail.com>
2021-12-29 12:11:24 +0100

    refactored font-size to %

--- view/includes/footer.php
+++ view/includes/footer.php
@@ -1,16 +1,18 @@
     </main>
         <footer class=" footer d-flex flex-wrap justify-content-between align-items-center py-3 my-4">
-            <div class="col-md-6 d-flex align-items-center">
-                <!-- <a href="/" class=" text-center mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-                  <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
-                </a> -->
-                <div class="text-muted text-center footername ">Copyright © 2021-2022 Pokeball Inc. All Rights Reserved.</div>
+            <div class="row footerContent">
+                <div class="col-md-6 d-flex align-items-center">
+                    <!-- <a href="/" class=" text-center mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                      <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
+                    </a> -->
+                    <div class="text-muted text-center footername ">Copyright © 2021-2022 Pokeball Inc. All Rights Reserved.</div>
+                </div>
+                <ul class="nav col-md-6 list-unstyled d-flex m-auto footerlogo">
+                    <li class="ms-2 "><a class="text-muted f-logo" href="http://twitter.com"><img src=".\assets\images\twitter.png" alt=""></a></li>
+                    <li class="ms-2 "><a class="text-muted f-logo" href="http://instagram.com"><img src=".\assets\images\instagram.png" alt=""></a></li>
+                    <li class="ms-2 "><a class="text-muted f-logo" href="http://facebook.com"><img src=".\assets\images\facebook.png" alt=""></a></li>
+                </ul>
             </div>
-            <ul class="nav col-md-6 list-unstyled d-flex m-auto footerlogo">
-                <li class="ms-2 "><a class="text-muted f-logo" href="http://twitter.com"><img src=".\assets\images\twitter.png" alt=""></a></li>
-                <li class="ms-2 "><a class="text-muted f-logo" href="http://instagram.com"><img src=".\assets\images\instagram.png" alt=""></a></li>
-                <li class="ms-2 "><a class="text-muted f-logo" href="http://facebook.com"><img src=".\assets\images\facebook.png" alt=""></a></li>
-            </ul>
         </footer>
 
         <!-- Option 1: Bootstrap Bundle with Popper -->
