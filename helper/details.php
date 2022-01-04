6da634f07a37cce7d8e0dbda74353641da84a663
zain Imtiaz <zain@zains-MacBook-Air.local>
2022-01-04 11:39:04 +0100

    serach bar

--- helper/details.php
+++ helper/details.php
@@ -5,13 +5,35 @@ Class Deatils{
 if (isset($_POST['submit'])) {
     $name = $_POST['search'];
 
-    $sql = 'SELECT * FROM Product WHERE name = :name';
+    $sql = 'SELECT * FROM Product WHERE `name` = :name';
     $stmt = $PDO->prepare($sql);
     $stmt->execute(['name' => $name]);
-    $row = $stmt->fetch();
+    var_dump($stmt);
+    $row = $stmt->fetchAll();
   } else {
-    header('location: helper/component.php');
+    header('location: ');
     exit();
   }
+
 }
 }
+?>
+
+
+<div class="container">
+    <div class="row mt-5">
+      <div class="col-5 mx-auto">
+        <div class="card shadow text-center">
+          <div class="card-header">
+            <h1><?php $name ?></h1>
+          </div>
+          <div class="card-body">
+            <h4><b>Description :</b> <?php $description ?></h4>
+            <h4><b>name :</b> <?php $name ?></h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
