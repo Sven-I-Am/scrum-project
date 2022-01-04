19b449a4accd4e4ec8bc6e805ea66f708b3708f1
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-01-04 09:56:25 +0100

    removed var_dump

--- controller/HomepageController.php
+++ controller/HomepageController.php
@@ -29,19 +29,19 @@ class HomepageController
 
     public function productCart($POST){
 
-        var_dump($POST['productId']);
         $date = new DateTime();
         $id = $POST['productId'];   
         $status = 1;   
                       
         ProductLoader::updateSoldStatus($this->db, $id, date_format($date, 'Y-m-d'), $status);
 
+        unset($_SESSION['cart']);
+
         if(!isset($_SESSION['cart'])){
             $_SESSION['cart'] = [];
         }            
         
         array_push($_SESSION['cart'], ProductLoader::readOneProduct($this->db, $id));              
-
     }
 }
 
