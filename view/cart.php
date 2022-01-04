b73b3c984110f5374a36b65ee167eb5b656f9427
unknown <nehalonkar27@gmail.com>
2022-01-04 10:16:37 +0100

    working on cart items

--- view/cart.php
+++ view/cart.php
@@ -1,1 +1,49 @@
-<?php require_once "homepage.php";?>
+<!-- Local CSS link -->
+<link rel="stylesheet" href="../view/style/style.css">
+<?php require 'includes/header.php';?>
+
+<div class="cartBody">
+<h3 class="cart">Your Selected Items</h3>
+<!-- <div class="row row-cols-1 row-cols-md-4 g-4 "> -->
+<table class=" test table table-bordered table-hover">
+  <thead>
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">Description</th>
+      <th scope="col">Price</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+    <td>Pokemon</td>
+      <td>its a soft toy</td>
+      <td>99.99</td>
+      <td><button type="submit" class="btn btn-outline-primary  btn-sm">Cancel</button></td>
+    </tr>
+    <tr>
+    <td>Pokemon</td>
+      <td>its a soft toy</td>
+      <td>99.99</td>
+      <td><button type="submit" class="btn btn-outline-primary  btn-sm">Cancel</button></td>
+    </tr>
+    <tr>
+    <td>Pokemon</td>
+      <td>its a soft toy</td>
+      <td>99.99</td>
+      <td><button type="submit" class="btn btn-outline-primary  btn-sm">Cancel</button></td>
+    </tr>
+    
+    <tr>
+    <td>Pokemon</td>
+      <td>its a soft toy</td>
+      <td>99.99</td>
+      <td><button type="submit" class="btn btn-outline-primary  btn-sm">Cancel</button></td>
+    </tr>
+   
+  </tbody>
+</table>
+</div>
+</div>
+
+<?php require 'includes/footer.php' ?>
