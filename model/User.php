6aa00d1045129612d95a300b0f05fb8782678e68
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-01-07 16:43:56 +0100

    added comments explain what each function does

--- model/User.php
+++ model/User.php
@@ -24,54 +24,55 @@ class User
         return $this->userid;
     }
 
-    // get and set userName
+    // get userName
 
     public function getUserName(): string
     {
         return $this->username;
         
     }
+    // set userName
     public function setUserName(string $data)
     {
         $this->username = $data;
     }
 
-    // get and set user email
+    // get user email
 
     public function getEmail(): string
     {
         return $this->email;
     }
-
+    // set user email
     public function setEmail(string $data)
     {
         $this->email = $data;
     }
 
-    // get and set seller's password
+    // get seller's password
 
     public function getPassword(): string
     {
         return $this->password;
     }
-
+    //set seller's password
     public function setPassword(string $data)
     {
         $this->password = $data;
     }
-
+    // check if the user is online
     public function checkOnline(PDO $PDO, int $id): int
     {
         $handler = $PDO->query('SELECT online FROM USER WHERE userid = ' . $id);
         $response = $handler->fetch();
         return intval($response['online']);
     }
-
+    // save in user table from database the user is online
     public function setOnline(PDO $PDO, int $id)
     {
         $PDO->query('UPDATE USER SET online = true WHERE userid = ' . $id);
     }
-
+    // save in user table from database the user is offline
     public function setOffline(PDO $PDO, int $id)
     {
         $PDO->query('UPDATE USER SET online = false WHERE userid = ' . $id);
